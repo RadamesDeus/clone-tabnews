f45dae23315d0bec81e7b9005f6b6eb2c5f5e498
Radames Deus <raihard@gmail.com>
2024-09-04 23:18:06 -0300

    Ajuste bug conexão aberta em `api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,32 +3,43 @@ import { join } from "path"
 import database from "infra/database"
 
 export default async function migrations( request, response ) {
+
+  if ( request.method != "GET" && request.method != "POST" ) {
+    return response.status( 405 )
+  }
   const dbClient = await database.getNewClient();
-  const configOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join( "infra", "migrations" ),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations"
-  }
 
-  if ( request.method === "GET" ) {
-    const pendingMigrations = await migrationRunner( configOptions )
+  try {
+
+    const configOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join( "infra", "migrations" ),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations"
+    }
+
+    if ( request.method === "GET" ) {
+      const pendingMigrations = await migrationRunner( configOptions )
+      return response.status( 200 ).json( pendingMigrations )
+    }
+
+    if ( request.method === "POST" ) {
+
+      const migratedMigrations = await migrationRunner( {
+        ...configOptions,
+        dryRun: false,
+      } )
+      return response.status( migratedMigrations.length > 0 ? 201 : 200 ).json( migratedMigrations )
+    }
+  } catch ( error ) {
+    console.error( error );
+    throw error;
+  } finally {
     dbClient.end();
-    return response.status( 200 ).json( pendingMigrations )
   }
 
-  if ( request.method === "POST" ) {
-    const migratedMigrations = await migrationRunner( {
-      ...configOptions,
-      dryRun: false,
-    } )
-    dbClient.end();
-    return response.status( migratedMigrations.length > 0 ? 201 : 200 ).json( migratedMigrations )
-  }
-
-  return response.status( 405 )
 
 
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -11,6 +11,5 @@ test( "GET to /api/v1/status should return 200", async () => {
   expect( bodyResponse.dependencies.database.max_connections ).toEqual( 100 )
   expect( bodyResponse.dependencies.database.opened_connections ).toEqual( 1 )
 
-  console.log( bodyResponse );
 } )
 
