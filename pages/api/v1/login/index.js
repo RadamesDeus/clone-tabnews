553972d70cfb5e22f39f80924788e8a60c3722ca
Radames Deus <raihard@gmail.com>
2024-11-17 00:01:53 -0300

    teste login

---
+++ pages/api/v1/login/index.js
@@ -0,0 +1,37 @@
+
+async function login( request, response ) {
+
+  const url = "https://www.dimepkairos.com.br/Dimep/Account/LogOn";
+  const usuario = "radames.bastos@dealernet.com.br"; // Substitua pelo valor do usuário
+  const senha = "hard1042,"; // Substitua pela senha
+
+  const dados = new URLSearchParams();
+  dados.append( "LogOnModel.UserName", usuario );
+  dados.append( "LogOnModel.Password", senha );
+  // dados.append("LogOnModel.Ip", ip); // Caso precise incluir o IP, descomente essa linha
+
+  // Fazendo a requisição POST com fetch
+  const result = await fetch( url, {
+    method: "POST",
+    body: dados,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  } )
+    .then( response => response.text() ) // Obtém a resposta como texto
+    .then( conteudo => {
+      console.log( conteudo ); // Exibe o conteúdo da resposta
+      // Verifique o conteúdo para determinar se o login foi bem-sucedido
+    } )
+    .catch( error => {
+      console.error( "Erro na requisição:", error );
+    } );
+
+
+  response.status( 200 ).setHeader( 'Content-Type', 'text/html' );
+  response.send( result );
+
+
+}
+
+export default login
