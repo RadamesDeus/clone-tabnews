f5438ae0ac884bceaf4dd608a712fd2899c27fd0
Radames Deus <raihard@gmail.com>
2024-12-28 22:19:42 -0300

    test: rename all description to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -4,22 +4,33 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("POST to /api/v1/migrations should return 201", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response.status).toBe(201);
+describe("POST  /api/v1/migrations", () => {
+  describe("Anonynous user", () => {
+    describe("Running pending migrations", () => {
+      test("for the first time", async () => {
+        const response = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+        expect(response.status).toBe(201);
 
-  const responseBody = await response.json();
+        const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  const result = await database.query("SELECT * from pgmigrations");
-  expect(result.rows.length).toBeGreaterThan(0);
+        expect(Array.isArray(responseBody)).toBe(true);
+        const result = await database.query("SELECT * from pgmigrations");
+        expect(result.rows.length).toBeGreaterThan(0);
+      });
 
-  const responseEmpty = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  const responseBodyEmpty = await responseEmpty.json();
+      test("for the second time", async () => {
+        const responseEmpty = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+        expect(responseEmpty.status).toBe(200);
+        const responseBodyEmpty = await responseEmpty.json();
 
-  expect(responseBodyEmpty.length).toBe(0);
-});
+        expect(responseBodyEmpty.length).toBe(0);
+      });
+    })
+  })
+})
+
+
