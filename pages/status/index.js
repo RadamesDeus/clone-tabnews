1493d918f90a817fd065464d1ce2d6a284818e7b
Radames Deus <raihard@gmail.com>
2024-12-31 13:59:53 -0300

    style: ajuste pretter file

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,23 +1,28 @@
-import userSWR from 'swr'
+import userSWR from "swr";
 async function fetchAPI(key) {
   const response = await fetch(key);
   const bodyResponse = await response.json();
   return bodyResponse;
 }
 
-
 function Status({ tipo }) {
   const { data, isLoading } = userSWR("/api/v1/status", fetchAPI, {
-    refreshInterval: 60000
-  })
+    refreshInterval: 60000,
+  });
   console.log(tipo);
 
   if (!isLoading) {
     let loading = "teste";
-    const { version, max_connections, opened_connections } = data.dependencies.database;
+    const { version, max_connections, opened_connections } =
+      data.dependencies.database;
     switch (tipo) {
       case "update_at":
-        loading = <div>última atualização: {new Date(data.update_at).toLocaleString("pt-BR")}</div>
+        loading = (
+          <div>
+            última atualização:{" "}
+            {new Date(data.update_at).toLocaleString("pt-BR")}
+          </div>
+        );
         break;
       case "version":
         loading = <div>Servidor Version: {version}</div>;
@@ -29,15 +34,12 @@ function Status({ tipo }) {
         loading = <div>Max de Connections {max_connections}</div>;
         break;
     }
-    return loading //JSON.stringify(data, null, 2)
-
+    return loading; //JSON.stringify(data, null, 2)
   }
-  return <div>Loading...</div>
+  return <div>Loading...</div>;
 }
 
 export default function statusPage() {
-
-
   return (
     <>
       <h1>Status</h1>
@@ -46,8 +48,6 @@ export default function statusPage() {
       <Status tipo="version" />
       <Status tipo="opened_connections" />
       <Status tipo="max_connections" />
-
-
     </>
   );
 }
